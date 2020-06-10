9c430e3680c2c6974aa38d95948c999683772ed1
Ruben Montanez <ruben.a.montanez@gmail.com>
2020-06-10 17:50:17 -0500

    some changes

--- main.js
+++ main.js
@@ -1,37 +1,21 @@
 "use strict"
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-// var coffees = [
-//     {id: 1, name: 'Light City', roast: 'light'},
-//     {id: 2, name: 'Half City', roast: 'light'},
-//     {id: 3, name: 'Cinnamon', roast: 'light'},
-//     {id: 4, name: 'City', roast: 'medium'},
-//     {id: 5, name: 'American', roast: 'medium'},
-//     {id: 6, name: 'Breakfast', roast: 'medium'},
-//     {id: 7, name: 'High', roast: 'dark'},
-//     {id: 8, name: 'Continental', roast: 'dark'},
-//     {id: 9, name: 'New Orleans', roast: 'dark'},
-//     {id: 10, name: 'European', roast: 'dark'},
-//     {id: 11, name: 'Espresso', roast: 'dark'},
-//     {id: 12, name: 'Viennese', roast: 'dark'},
-//     {id: 13, name: 'Italian', roast: 'dark'},
-//     {id: 14, name: 'French', roast: 'dark'},
-// ];
 var coffees = [
-    {id: 1, name: 'Tent City', roast: 'light', image:'img/dirtyjoe.jpg'},
-    {id: 2, name: 'Carboard House', roast: 'light'},
-    {id: 3, name: 'Pigeon Drop', roast: 'light'},
-    {id: 4, name: "Where's My Wife?", roast: 'medium'},
-    {id: 5, name: 'Hobo Joe', roast: 'medium'},
-    {id: 6, name: 'Where Am I?', roast: 'medium'},
-    {id: 7, name: 'Shoeless Select', roast: 'dark'},
-    {id: 8, name: 'Tent Post', roast: 'dark'},
-    {id: 9, name: 'Pennyfight Premier', roast: 'dark'},
-    {id: 10, name: 'Mismatch Mix', roast: 'dark'},
-    {id: 11, name: 'Barrelfire Roast', roast: 'dark'},
-    {id: 12, name: 'Beenie Weenie Sour', roast: 'dark'},
-    {id: 13, name: "It's Getting Really Really Dark", roast: 'dark'},
-    {id: 14, name: "Busker's Brew", roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
 ];
 
 // function renderCoffee(coffee) {
